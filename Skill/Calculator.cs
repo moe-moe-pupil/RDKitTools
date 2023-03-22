a9070c6002a838a846428e834834845621871d4d
moe-moe-pupil <dhtmitmi@gmail.com>
2023-03-22 21:07:19 +0800

    chore: protocol errata

--- Skill/Calculator.cs
+++ Skill/Calculator.cs
@@ -1,7 +1,7 @@
 ﻿// --------------------------------------------------------------------------------------------------------------
 // <copyright file="Calculator.cs" company="RDKitTools Technologies and contributors.">
 // 此源代码的使用受 MIT LICENSE 许可证的约束, 可以在以下链接找到该许可证.
-// Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
+// Use of this source code is governed by the MIT license that can be found through the following link.
 // https://github.com/moe-moe-pupil/RDKitTools/blob/main/LICENSE
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------
